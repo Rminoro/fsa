c738c052017f0528a0057a851ffb528c8c48198c
rminoro <rminoro@outlook.com>
2023-11-07 01:41:48 -0300

    estilizacação chatbot

--- src/app/link/Chatbot/page.jsx
+++ src/app/link/Chatbot/page.jsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
+import style from "../Chatbot/style.css";
 import ellipse from "../../../../public/img/ellipse-1.png"
 import iconePortinho from "../../../../public/img/iconePortinho.png";
+import retangle2 from "../../../../public/img/rectangle-12.png";
+import close from "../../../../public/img/close.png";
 
 
 
@@ -11,15 +14,18 @@ import iconePortinho from "../../../../public/img/iconePortinho.png";
 export default function Chatbot() {
   return (
     <div id="scrollableContainer" style={{ width: '100%', height: 'calc(100vh - 60px)', overflow: 'auto' }}>
-      <div>
-        <div >
-          <div >
+      <div className="chatbot">
+        <div className="chatbot-px">
+          <div className="overlap">
             <img src="/iconePortinho.png" alt="iconePortinho" />
             <Image src="/img/iconePortinho.png" alt="iconePortinho" width={200} height={200} /> 
-            <div >
-              <div >
-                <img src="/retangle2.png" alt="Retangulo fundo de texto" />
-                <p >
+            <div className="apresentao-do">
+              <div className="overlap-group">
+                {/* <img src="/retangle2.png" alt="Retangulo fundo de texto" /> */}
+                {/* <Image id="motorista" src="/img/motorista.png" alt="Motorista de caminhão" width={200} height={200}></Image> */}
+                {/* <Image id="retangle2" src="/img/retangle2.png" alt="Retangulo fundo de texto" width={200} height={200}></Image> */}
+                {/* <Image id="retangle2" src="/img/retangle2.png" alt="Retangulo fundo de texto" width={200} height={200} /> */}
+                <p className="para-isso">
                   Para isso, apresentamos a vocês o Portinho, nosso mascote virtual que será responsável pelo atendimento
                   via Chatbot.
                   <br />
@@ -33,38 +39,41 @@ export default function Chatbot() {
 
             <img src="/retangle1.png" alt="Informaes" />
 
-            <div >
-              <div >
-                <div >
-                  <div >
+            <div className="tela-chatbot" >
+              <div className="div">
+                <div className="overlap-2">
+                  <div className="ellipse-wrapper">
                   <Image src="/img/ellipse-1.png" alt="Ellipse" width={200} height={200} /> 
                     {/* <img  src="/ellipse-1.png" alt="Ellipse" /> */}
                   </div>
-                  <div >Portinho</div>
-                  <img  src="/close.png" alt="Close" />
+                  {/* <div className="text-wrapper" >Portinho</div> */}
+                  <Image className="close" src="/img/close.png" alt="close" width={200} height={200} /> 
+                  
                 </div>
-                <div >
-                  <div  />
-                  <div>Digite sua mensagem...</div>
-                  <img  src="/emote.png" alt="Emote" />
-                  <img  src="/email-send.png" alt="Email send" />
-                  <img src="/attach.png" alt="Attach" />
-                  <img  src="/audio.png" alt="Audio" />
+                <div  className="overlap-group-2">
+                  <div  className="caixa-texto"/>
+                  <div className="text-wrapper-2">Digite sua mensagem...</div>
+               
+                  <Image className="emote" src="/img/emote.png" alt="emote" width={200} height={200} /> 
+                  
+                  <Image className="email-send" src="/img/email-send.png" alt="email" width={200} height={200} />
+                  
+                  <Image className="audio" src="/img/audio.png" alt="audio" width={200} height={200} />
                 </div>
-                <div>
-                  <p >Olá! Sou o Portinho, o bot da Porto Help!</p>
+                <div className="msg-portinho">
+                  <p className="p">Olá! Sou o Portinho, o bot da Porto Help!</p>
                 </div>
-                <div >
-                  <p >No que posso te ajudar?</p>
+                <div className="div-wrapper">
+                  <p className="p">No que posso te ajudar?</p>
                 </div>
-                <div >
-                  <div >Olá Portinho!</div>
+                <div className="msg-cliente">
+                  <div className="text-wrapper-3">Olá Portinho!</div>
                 </div>
-                <div >
-                  <p >Preciso de um guincho urgente</p>
+                <div className="msg-cliente-2">
+                  <p className="text-wrapper-3">Preciso de um guincho urgente</p>
                 </div>
-                <div >
-                  <p >Estou enviando um modal para você agora mesmo</p>
+                <div className="msg-portinho-2">
+                  <p className="text-wrapper-4">Estou enviando um modal para você agora mesmo</p>
                 </div>
               </div>
             </div>
